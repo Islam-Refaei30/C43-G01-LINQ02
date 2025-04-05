72882c21d037454c538aee5f290bd904c41e970c
islam-refaei <i.refaei.refaei.saadawi@gmail.com>
2025-04-05 16:38:21 +0200

    Add project files.

---
+++ Assignment/Program.cs
@@ -0,0 +1,12 @@
+﻿using static Assignment.ListGenerator;
+namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test Assignment");
+            Console.WriteLine(ProductList[0]);
+        }
+    }
+}
---
+++ Demo/Program.cs
@@ -0,0 +1,12 @@
+﻿using static Demo.ListGenerator;
+namespace Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test Demo");
+            Console.WriteLine(ProductList[0]);
+        }
+    }
+}
